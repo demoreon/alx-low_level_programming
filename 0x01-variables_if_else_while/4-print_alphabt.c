fc153fbf05388bb0630be5ccf7909d8dd5643511
demoreon <demoreon@gmail.com>
2022-09-10 00:59:39 +0100

    t4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - prints the alphabet in Alphabet Soup
+*
+*Return: ALways 0
+*/
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		if (a != 'e' && a != 'q')
+			putchar(a);
+	putchar('\n');
+	return (0);
+}
