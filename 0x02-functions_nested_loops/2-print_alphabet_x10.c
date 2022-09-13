8c17f43c2874a92c9e94a873a3425723359cd1e6
demoreon <demoreon@gmail.com>
2022-09-13 21:33:20 +0100

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints lowercase X10
+ *
+ * Returns: Void
+ */
+
+void print_alphabet_x10(void)
+{
+	char x;
+	int y;
+
+	for (y = 1; y <= 10; y++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 void print_alphabet(void);
 void _putchar(char x);
+void print_alphabet_x10(void);
