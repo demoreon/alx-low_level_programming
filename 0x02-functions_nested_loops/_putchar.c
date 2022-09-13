c39f172c4f65232133b0543fac51569cf56bb9dc
demoreon <demoreon@gmail.com>
2022-09-13 18:31:40 +0100

    1

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,13 +1,11 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
+ * _putchar - output a single character
+ * Return: void
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+void _putchar(char c)
 {
-	return (write(1, &c, 1));
+	write(1, &c, 1);
 }
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - test magna
+
+ * Return: 0 End
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
