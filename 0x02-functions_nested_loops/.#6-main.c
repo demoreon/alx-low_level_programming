6e46056030967f3054c7662a96362b0d046a730b
demoreon <demoreon@gmail.com>
2022-09-14 01:52:55 +0100

    t6

--- 0x02-functions_nested_loops/.#6-main.c
+++
@@ -1,1 +0,0 @@
-demoreon@DESKTOP-PF23UF8.2043
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -10,11 +10,11 @@ int _abs(int n)
 
 	if (n >= 0)
 	{
-		return(_putchar(n));
+		return(n);
 	}
 	else
 	{
 		n = n * -1;
-		return(_putchar(n));
-	}
+		return(n);
+       	}
 }
