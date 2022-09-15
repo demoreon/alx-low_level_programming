892597e7c7980c70ec3c36aba1d59e0d56c1bbe5
demoreon <demoreon@gmail.com>
2022-09-15 22:56:14 +0100

    t3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - checks for digit
+ * @a: Int a
+ * @b: Int b
+ * Return: 1 if true or 0 for false
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+		_putchar(n);
+	_putchar('\n');
+}
