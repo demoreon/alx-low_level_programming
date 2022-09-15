16dbce2aab6b65e44e2d4727c711c517da14791b
demoreon <demoreon@gmail.com>
2022-09-15 22:41:40 +0100

    t2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - checks for digit
+ * @a: Int a
+ * @b: Int b
+ * Return: 1 if true or 0 for false
+ */
+
+int mul(int a, int b)
+{
+	int x;
+
+	x = a * b;
+	return (x);
+}
