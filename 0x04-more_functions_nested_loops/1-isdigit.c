97ccb34239e4fd76f0494947a76ef56e9a9381b0
demoreon <demoreon@gmail.com>
2022-09-15 22:31:01 +0100

    t1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - checks for digit
+ * @c: Checker
+ * Return: 1 if true or 0 for false
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
