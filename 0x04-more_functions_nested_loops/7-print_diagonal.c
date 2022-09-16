207c9fde665211fa2506fb1226662d8fdc958e07
demoreon <demoreon@gmail.com>
2022-09-16 01:22:45 +0100

    t7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,21 +5,23 @@
  */
 void print_diagonal(int n)
 {
-	int i;
-	int j;
+	int i, j;
 
 	if (n <= 0)
-	{
 		_putchar('\n');
-	}
-	else
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
+		for (j = 0; j <= i; j++)
 		{
-			_putchar('\\');
-			_putchar('\n');
-			for (j = 0; j < i; j++)
+			if (i == j)
+			{
+				_putchar('\\');
+				_putchar('\n');
+			}
+			else
+			{
 				_putchar(' ');
+			}
 		}
 	}
 }
