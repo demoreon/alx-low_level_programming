3f5667e00607f0fe4793d97942c4e505fdf48504
demoreon <demoreon@gmail.com>
2022-10-12 14:33:59 +0100

    t1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,9 +11,11 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	int i;
+
 	if (array && size && action)
 	{
-		for (unsigned int i = 0; i < size ; i++)
+		for (i = 0; i < size ; i++)
 			action(array[i]);
 	}
 }
