81133a9930df450b07428f78fdf8924cec064fb1
demoreon <demoreon@gmail.com>
2022-10-12 23:33:21 +0100

    t3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,4 @@
 #include "3-calc.h"
-#include <string.h>
 
 /**
  * get_op_func - selects the correct function to perform
@@ -22,7 +21,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (i < 10)
 	{
-		if (strcmp(s, ops[i].op) == 0)
+		if (s[0] == ops->op[i])
 			break;
 		i++;
 	}
